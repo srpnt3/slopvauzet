081a2cb81c7454805ed82d34c7787b37149e6057
Andri Florin <florina@student.ethz.ch>
2024-10-10 20:38:24 +0200

    Added link to repo

--- frontend/src/components/AppHeader.js
+++ frontend/src/components/AppHeader.js
@@ -12,7 +12,7 @@ export default function AppHeader() {
             <p className="Text">
                 Hello Hackers!<br/><br/>
                 We've provided you with an example app to get you started quickly. Feel free to change anything
-                you'd like or yeet it completely.
+                you'd like or yeet it completely. <a className="Link" href="https://gitlab.ethz.ch/vseth/1100-fv/1116-vis/vc2/sip-vis-vc2-hackathon/template">Link</a> to the Gitlab repository.
                 <br/><br/>Cheers!<br/>Your Hackathon Team
             </p>
             <span className="Spacer"/>
