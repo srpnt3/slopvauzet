6c292138b93c9fe4f09119da28ce80fd76b3a510
jktham <jonas.tham@gmx.ch>
2025-10-11 19:16:53 +0200

    timetable grid setup

--- frontend/src/components/Studyplan.tsx
+++ frontend/src/components/Studyplan.tsx
@@ -3,7 +3,16 @@ import "./Studyplan.css";
 function Studyplan() {
   return (
     <div className="studyplan">
-      <div>studyplan</div>
+      <div className="column">
+        <div className="info">programme</div>
+        <div className="info">semester</div>
+        <div className="info">credits</div>
+      </div>
+      <div className="column">
+        <div className="credits_container">
+          
+        </div>
+      </div>
     </div>
   );
 }
--- frontend/src/components/Timetable.tsx
+++ frontend/src/components/Timetable.tsx
@@ -1,10 +1,25 @@
+import type { JSX } from "react";
 import type { Course } from "./Search";
 import "./Timetable.css";
 
 function Timetable({courses}: {courses: Course[]}) {
+  
+  const renderTimetable = () => {
+    let cells: JSX.Element[] = [];
+    ["(" + courses.length + ")", "mon", "tue", "wed", "thu", "fri"].map(d => cells.push(<div className="cell day">{d}</div>));
+    for (let h=8; h<22; h++) {
+      cells.push(<div className="cell time">{h.toString().padStart(2, "0")} - {(h+1).toString().padStart(2, "0")}</div>)
+      for (let i=0; i<5; i++) {
+        cells.push(<div className="cell"></div>);
+      }
+    }
+    
+    return cells;
+  }
+
   return (
     <div className="timetable">
-      <div>todo timetable here {courses.length} courses here do put here {courses.length} of them</div>
+      {renderTimetable()}
     </div>
   );
 }
