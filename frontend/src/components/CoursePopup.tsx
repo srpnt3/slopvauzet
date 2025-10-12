32a27416896ef5939a720e2e89b2607bdfd9eede
srpnt3 <27972286+srpnt3@users.noreply.github.com>
2025-10-12 04:43:44 +0200

    finished styling epic based mega based sigma pilled course data page H-yuck H-yuck

--- frontend/src/components/CoursePopup.tsx
+++ frontend/src/components/CoursePopup.tsx
@@ -1,7 +1,7 @@
 import "./CoursePopup.css";
 import type { Course } from "./Search";
 import Badge from "./Badge.tsx";
-import { useMemo } from "react";
+import { Fragment, useMemo } from "react";
 
 function CoursePopup({course, setCoursePopup}: { course: Course, setCoursePopup: (course: Course | undefined) => void }) {
 
@@ -16,23 +16,39 @@ function CoursePopup({course, setCoursePopup}: { course: Course, setCoursePopup:
 				<Badge title={course.offered_in[0].section} className={"bg-bg3 text-fg3"}>{course.offered_in[0].section}</Badge>
 				<Badge className={"bg-bg3 text-fg3"}>{course.performance_assessment[0].ects_credits}kp</Badge>
 			</div>
-			<div className="grid grid-cols-2 overflow-y-scroll">
-				{courseData.map((item, index) => (
-					<div key={index}>{item.sectionTitle}</div>
-				))}
+			<div className="flex flex-col gap-12 overflow-y-scroll">
+				{courseData.map((s, i) => (<div key={i}>
+					{s.sectionTitle && <h2 className={"ml-48 mb-4 font-medium"}>{s.sectionTitle}</h2>}
+					<div className={"w-full max-w-[64rem] grid grid-cols-[10rem_auto] gap-x-8 gap-y-4"}>
+						{s.content.map((c, i) => (<Fragment key={i}>
+							<div className={"text-xs text-fg2"}>{c.name}</div>
+							<div className={"text-xs"}>{c.data}</div>
+						</Fragment>))}
+					</div>
+				</div>))}
 			</div>
 		</div>
 	);
 }
 
 export function formatCouseData(course: Course): {
-	sectionTitle: string;
+	sectionTitle?: string;
 	content: {
 		name: string;
 		data: string;
 	}[]
 }[] {
-	return []
+	return [{
+		sectionTitle: "Catalogue Data",
+		content: [{
+			name: "Abstract",
+			data: "This lecture provides deeper knowledge on the possible applications of  virtual reality, its basic technolgy, and future research fields. The  goal is to provide a strong knowledge on Virtual Reality for a possible  future use in business processes."
+		},
+			{
+				name: "Abstract",
+				data: "This lecture provides deeper knowledge on the possible applications of  virtual reality, its basic technolgy, and future research fields. The  goal is to provide a strong knowledge on Virtual Reality for a possible  future use in business processes."
+			}]
+	}]
 }
 
 export default CoursePopup;
